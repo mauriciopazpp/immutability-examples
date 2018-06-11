6424d4bcef7488dd0a3816192761d9122fd8ecca
Mauricio Paz <mauriciopazdesign@gmail.com>
2018-06-11 00:24:35 -0300

    React immutable example

--- examples with libraries/react native/immutable/src/App.js
+++ examples with libraries/react native/immutable/src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { value: 10 }
+  }
+
+  setNewValue(newValue) {
+    this.setState({ value: newValue})
+  }
+
+  componentDidMount() {
+    this.setNewValue(1)
+  }
+
   render() {
     return (
       <div className="App">
-        
+        <h1>
+        { this.state.value }
+        </h1>
       </div>
     );
   }
