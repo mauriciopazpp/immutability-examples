ebd8b2c2b85c25f7f168717d2cc6d098ce521872
Mauricio Paz <mauriciopazdesign@gmail.com>
2018-06-10 23:15:08 -0300

    All folders languages moved to root

--- examples of immutability/java/Immutable.java
+++
@@ -1,7 +0,0 @@
-public class Immutable {
-    public static void main(String[] args) {
-    	String s = "ABC";
-	  	String b = s.toLowerCase();
-        System.out.println("A variável " + s + " é diferente de " + b);
-    }
-}
