037f7346dbe1f81865469b5d8d65d52c6eef5f55
Mauricio Paz <mauriciopazdesign@gmail.com>
2018-06-10 23:13:56 -0300

    Files moved to root

---
+++ java/Immutable.java
@@ -0,0 +1,7 @@
+public class Immutable {
+    public static void main(String[] args) {
+    	String s = "ABC";
+	  	String b = s.toLowerCase();
+        System.out.println("A variável " + s + " é diferente de " + b);
+    }
+}
---
+++ java/readme.md
@@ -0,0 +1,12 @@
+Para executar, digite:
+
+Compile:
+`````
+ javac Immutable.java
+`````
+
+Execute:
+`````
+java Immutable
+
+`````
